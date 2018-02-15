6536361a02acff4145197f5f152403853a10c69c
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2018-02-15 19:19:43 -0200

    Add new states for the game

--- Text 101/Assets/Scripts/TextController.cs
+++ Text 101/Assets/Scripts/TextController.cs
@@ -17,14 +17,14 @@ public class TextController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (myState == States.Cell) StateCell();
-		//else if (myState == States.Sheets0) StateSheets0();
-		//else if (myState == States.Sheets1) StateSheets1();
-		//else if (myState == States.Lock0) StateLock0();
-		//else if (myState == States.Lock1) StateLock1();
-		//else if (myState == States.Mirror) StateMirror();
-		//else if (myState == States.CellMirror) StateCellMirror();
-		//else if (myState == States.Freedom) StateFreedom();
+		if(myState == States.Cell) StateCell();
+		else if(myState == States.Sheets0) StateSheets0();
+		//else if(myState == States.Sheets1) StateSheets1();
+		else if(myState == States.Lock0) StateLock0();
+		//else if(myState == States.Lock1) StateLock1();
+		else if(myState == States.Mirror) StateMirror();
+		//else if(myState == States.CellMirror) StateCellMirror();
+		//else if(myState == States.Freedom) StateFreedom();
 	}
 
 	void StateCell() {
@@ -32,8 +32,28 @@ public class TextController : MonoBehaviour {
 					"some dirty sheets on the bed, a mirror on the wall, and the door " +
 					"is locked from the outside.\n\n" +
 					"Press S to view Sheets, M to view Mirror and L to view Lock";
-		if (Input.GetKeyDown (KeyCode.S)) myState = States.Sheets0;
-		else if (Input.GetKeyDown (KeyCode.M)) myState = States.Mirror;
-		else if (Input.GetKeyDown (KeyCode.L)) myState = States.Lock0;
+		if(Input.GetKeyDown(KeyCode.S)) myState = States.Sheets0;
+		else if(Input.GetKeyDown(KeyCode.M)) myState = States.Mirror;
+		else if(Input.GetKeyDown(KeyCode.L)) myState = States.Lock0;
 	}
+
+    void StateMirror() {
+        text.text = "The dirty old mirror on the wall seems loose.\n\n" +
+                    "Press T to Take the mirror, or R to Return to cell";        if(Input.GetKeyDown(KeyCode.T)) myState = States.CellMirror;        else if(Input.GetKeyDown(KeyCode.R)) myState = States.Cell;
+    }
+
+    void StateSheets0() {
+        text.text = "You can't believe you sleep in these things. Surely it's " +
+                    "time somebody changed them. The pleasures of prison life " +
+                    "I guess!\n\n" +
+                    "Press R to Return to roaming your cell";
+        if(Input.GetKeyDown(KeyCode.R)) myState = States.Cell;
+    }
+
+    void StateLock0() {
+        text.text = "This is one of those button locks. You have no idea what the " +
+                    "combination is. You wish you could somehow see where the dirty " +
+                    "fingerprints were, maybe that would help.\n\n" +
+                    "Press R to Return to roaming your cell";        if(Input.GetKeyDown(KeyCode.R)) myState = States.Cell;
+    }
 }
