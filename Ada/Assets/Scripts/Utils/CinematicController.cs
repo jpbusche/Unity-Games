86ebdb8b02938a7908c7c21bae33649fdd370e00
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2018-07-16 22:59:41 -0300

    Adding cinematic time

---
+++ Ada/Assets/Scripts/Utils/CinematicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CinematicController : MonoBehaviour {
+
+    public string sceneName;
+    private float cinematicTimer = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cinematicTimer -= Time.deltaTime;
+        if(Input.GetKeyDown("space")) {
+            cinematicTimer = 0.0f;
+        }
+        if(cinematicTimer <= 0.0f) {
+            SceneManager.LoadScene(sceneName);
+        }
+	}
+}
