43e2b9805d792d70f6955fd28e6b973b2f783dae
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-26 20:03:55 -0200

    Adding gameplay code

---
+++ Number Wizard/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gameplay : MonoBehaviour {
+
+
+    public int max;
+    public int min;
+    public Text guessText;
+    int guess;
+
+	// Use this for initialization
+	void Start () {
+        CalculateGuess();
+    }
+
+    void CalculateGuess() {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher() {
+        min = guess + 1;
+        CalculateGuess();
+    }
+
+    public void OnPressLower() {
+        max = guess - 1;
+        CalculateGuess();
+    }
+}
