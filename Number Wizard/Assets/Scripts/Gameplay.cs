dc94a5610c43bce9257d7235ddabc9f1bea3861f
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-26 22:09:04 -0200

    Adding number max of guess

--- Number Wizard/Assets/Scripts/Gameplay.cs
+++ Number Wizard/Assets/Scripts/Gameplay.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Gameplay : MonoBehaviour {
 
 
     public int max;
     public int min;
+    public int numberGuess;
     public Text guessText;
     int guess;
 
 	// Use this for initialization
 	void Start () {
         CalculateGuess();
+    }
+
+    void Update() {
+        if(numberGuess == 0) {
+            SceneManager.LoadScene("Lose");
+        }
     }
 
     void CalculateGuess() {
@@ -24,10 +32,12 @@ public class Gameplay : MonoBehaviour {
     public void OnPressHigher() {
         min = guess + 1;
         CalculateGuess();
+        numberGuess -= 1;
     }
 
     public void OnPressLower() {
         max = guess - 1;
         CalculateGuess();
+        numberGuess -= 1;
     }
 }
