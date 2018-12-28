f8f423026eddd8c4514380b52e455ec10f870b6f
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-28 19:34:52 -0200

    Creating Game Over and Congratulations scenes

---
+++ Block Breaker/Assets/Scripts/LoseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.name == "Ball") {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
