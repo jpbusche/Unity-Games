a297659ae23acd50e699397e6773f77a4a2fe85c
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-28 21:26:24 -0200

    Moving paddle

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    public float screenUnits = 16;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float posX = Input.mousePosition.x / Screen.width * screenUnits;
+        Debug.Log(posX);
+        transform.position = new Vector2(posX, transform.position.y);
+    }
+}
