0baa8ec51c853e731458024c73082466737db90f
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-29 18:22:12 -0200

    Level Design of 3 levels

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -7,12 +7,18 @@ public class Block : MonoBehaviour {
     public Sprite[] blockSprites;
     public int lives = 2;
 
+    SpriteRenderer render;
+
+    void Start() {
+        render = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.name == "Ball") {
             if(lives == 0) Destroy(gameObject);
             else {
                 lives--;
-                GetComponent<SpriteRenderer>().sprite = blockSprites[lives];
+                render.sprite = blockSprites[lives];
             }
         }
     }
