734e0ca4daefa668e75e3322fe53b53a4d74e021
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-28 23:21:51 -0200

    Creating Level 1

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -7,15 +7,11 @@ public class Block : MonoBehaviour {
     public Sprite[] blockSprites;
     public int lives = 2;
 
-    void Start() {
-        GetComponent<SpriteRenderer>().sprite = blockSprites[0];
-    }
-
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.name == "Ball") {
             if(lives == 0) Destroy(gameObject);
             lives--;
-             GetComponent<SpriteRenderer>().sprite = blockSprites[lives + 1];
+            GetComponent<SpriteRenderer>().sprite = blockSprites[lives];
         }
     }
 
