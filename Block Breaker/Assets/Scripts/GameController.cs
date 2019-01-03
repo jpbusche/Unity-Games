edad4746ffaf531ca31b27e0d423299e01bc527b
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2019-01-03 14:59:23 -0200

    Adding new level and number of blocks

--- Block Breaker/Assets/Scripts/GameController.cs
+++ Block Breaker/Assets/Scripts/GameController.cs
@@ -8,25 +8,31 @@ public class GameController : MonoBehaviour {
     public GameObject blocks;  
     public Button nextButton;
     public Text messageText;
+    public Text blocksText;
 
     Ball ball;
     Paddle paddle;
+    int numberOfBlocks;
 
     void Start() {
         ball = FindObjectOfType<Ball>();
         paddle = FindObjectOfType<Paddle>();
         messageText.gameObject.SetActive(false);
         nextButton.gameObject.SetActive(false);
+        numberOfBlocks = blocks.transform.childCount;
+        blocksText.text = "Blocks       " + numberOfBlocks.ToString();
     }
 
     // Update is called once per frame
     void Update() {
-        int numberOfBlocks = blocks.transform.childCount;
+        numberOfBlocks = blocks.transform.childCount;
+        blocksText.text = "Blocks       " + numberOfBlocks.ToString();
         if(numberOfBlocks <= 0) {
             if(ball != null) Destroy(ball.gameObject);
             if(paddle != null)Destroy(paddle.gameObject);
             messageText.gameObject.SetActive(true);
             nextButton.gameObject.SetActive(true);
+            blocksText.gameObject.SetActive(false);
         }
     }
 }
