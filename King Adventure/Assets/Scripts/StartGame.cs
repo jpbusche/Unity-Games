55b7e6f8859d49440ac44f0d59e2fb6796c92e03
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2019-02-19 14:52:46 -0300

    Adding menu scene

---
+++ King Adventure/Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    
+    // Start is called before the first frame update
+    void Start() {
+        int totalCoins = 0;
+        PlayerPrefs.SetInt("Player Coins", totalCoins);
+    }
+
+}
