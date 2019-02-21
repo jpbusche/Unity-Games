96aa2660ff3885b7a20455000f2b0ee458864733
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2019-02-20 21:13:48 -0300

    Adding lifebar

--- King Adventure/Assets/Scripts/Game.cs
+++ King Adventure/Assets/Scripts/Game.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class Game : MonoBehaviour {
 
     public Text endText;
+    public Image lifeBar;
+    public Sprite[] sprites;
 
     King king;
     Scene scene;
@@ -15,16 +17,19 @@ public class Game : MonoBehaviour {
     void Start() {
         king = FindObjectOfType<King>();
         scene = SceneManager.GetActiveScene();
+        lifeBar.sprite = sprites[3];
         endText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update() {
+        lifeBar.sprite = sprites[king.health];
         if(king.won) {
             endText.text = "You Won                            Press Space";
             endText.gameObject.SetActive(true);
             if(Input.GetKeyDown(KeyCode.Space)) SceneManager.LoadScene(scene.buildIndex + 1);
         } else if(king == null) {
+            lifeBar.sprite = sprites[0];
             endText.text = "You Died                            Press Space";
             endText.gameObject.SetActive(true);
             if(Input.GetKeyDown(KeyCode.Space)) SceneManager.LoadScene(scene.buildIndex);
